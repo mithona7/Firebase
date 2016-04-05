e9d41dd597f5a1debcfe43e65e1e8e5088fd9f56
Kim Mithona <kimmithona7@gmail.com>
2016-04-05 19:35:39 +0700

    Initial Commit

---
+++ TestingFireBase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingFireBase
+//
+//  Created by Kim Mithona on 5/4/2559 BE.
+//  Copyright © 2559 BE Kim Mithona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
